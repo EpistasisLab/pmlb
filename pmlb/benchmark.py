8aac080567c50c7cb0ac4db6d422dfee0cba63d4
athril <patryk.orzechowski@gmail.com>
2017-03-27 17:04:21 -0400

    Added tool for automatic generation of dataset reports as well as summary report about PMLB

---
+++ pmlb/benchmark.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2017 Patryk Orzechowski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+
+
+import glob, os
+import csv
+import pandas as pd
+from collections import Counter
+from pmlb import fetch_data
+from pmlb import dataset_names
+
+def imbalance_metrics(data):
+    """ Computes imbalance metric for a given dataset. 
+    Imbalance metric is equal to 0 when a dataset is perfectly balanced (i.e. number of in each class is exact).
+    :param data : pandas.DataFrame 
+        A dataset in a panda's data frame
+    :returns int 
+        A value of imbalance metric, where zero means that the dataset is perfectly balanced and the higher the value, the more imbalanced the dataset.
+    """
+    if not data:
+        return 0
+    #imb - shows measure of inbalance within a dataset
+    imb = 0 
+    num_classes=float(len(Counter(data)))
+    for x in Counter(data).values():
+        p_x = float(x)/len(data)
+        if p_x > 0:
+            imb += (p_x - 1/num_classes)*(p_x - 1/num_classes)
+    #worst case scenario: all but 1 examplars in 1st class, the remaining one in 2nd class
+    worst_case=(num_classes-1)*pow(1/num_classes,2) + pow(1-1/num_classes,2)
+    return (num_classes,imb/worst_case)
+
+
+
+def determine_endpoint_type(features):
+    """ Determines the type of an endpoint
+    :param features: pandas.DataFrame
+        A dataset in a panda's data frame
+    :returns string
+        string with a name of a dataset
+    """    
+    counter={k.name: v for k, v in features.columns.to_series().groupby(features.dtypes).groups.items()}
+    if(len(features.groupby('class').apply(list))==2):
+        return('binary')
+    if ('float64' in counter):
+        return ('float')
+    return ('integer')
+    #binary=len(set(features.loc[:, (features<=1).all(axis=0)].columns.values) \
+    #           & set(features.loc[:, (features>=0).all(axis=0)].columns.values) \
+    #           & set(counter['int64']))
+    
+
+def count_features_type(features):
+    """ Counts three different types of features (float, integer, binary).
+    :param features: pandas.DataFrame
+        A dataset in a panda's data frame
+    :returns a tuple (binary, integer, float)
+    """    
+    counter={k.name: v for k, v in features.columns.to_series().groupby(features.dtypes)}
+    binary=0
+    if ('int64' in counter):
+        binary=len(set(features.loc[:, (features<=1).all(axis=0)].columns.values) & set(features.loc[:, (features>=0).all(axis=0)].columns.values) & set(counter['int64']))    
+    return (binary,len(counter['int64'])-binary if 'int64' in counter else 0,len(counter['float64']) if 'float64' in counter else 0)
+
+
+def generate_description(dataset_name, local_cache_dir=None):
+    """Generates desription for a given dataset in its README.md file in a dataset local_cache_dir file.
+    
+    :param dataset_name: str
+        The name of the data set to load from PMLB.
+    :param local_cache_dir: str (required)
+        The directory on your local machine to store the data files.
+        If None, then the local data cache will not be used.
+    """
+    
+    assert (local_cache_dir!=None)
+    readme_file = open(os.path.join(local_cache_dir,'datasets',dataset_name,'README.md'), 'wt')
+    try:
+        df = fetch_data(dataset_name)
+        #determine all required values
+        feat=count_features_type(df.ix[:, df.columns != 'class'])
+        endpoint=determine_endpoint_type(df.ix[:, df.columns == 'class'])
+        mse=imbalance_metrics(df['class'].tolist())
+        #proceed with writing
+        readme_file.write('Dataset name: %s\n' % dataset_name)
+        readme_file.write('#instances: %s\n' % str(len(df.axes[0])))
+        readme_file.write("#features: %s\n" % str(len(df.axes[1])-1))
+        readme_file.write("  #binary_features: %s\n" % feat[0])
+        readme_file.write("  #integer_features: %s\n" % feat[1])
+        readme_file.write("  #float_features: %s\n" % feat[2])
+        readme_file.write("Endpoint type: %s\n" % endpoint)
+        readme_file.write("#Classes: %s\n" % int(mse[0]))
+        readme_file.write("Imbalance metric: %s\n" % mse[1])
+    except IOError as err:
+        print(err)
+    finally:
+        readme_file.close()
+
