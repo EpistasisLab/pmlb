351e382abe424422c486935d81abba465fadf1eb
lacava <williamlacava@gmail.com>
2020-05-14 17:55:40 -0400

    updates write_metadata to generate metadata.yaml files (issue #26) using formating from issue #22 and adds summary stats

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,14 +1,14 @@
 import glob
 import yaml
 import pandas as pd
-yaml_files = glob.glob('datasets/*/*.yaml')
+import pdb
+yaml_files = glob.glob('datasets/*/*/*.yaml')
 
 
 def test_all_yaml_files():
     "Check basic information in yaml files."
     for yf in yaml_files:
-        folder_name = yf.split("/")[1]
-        file_name = "datasets/{0}/{0}.tsv.gz".format(folder_name)
+        folder_name = yf.split("/")[-2]
 
         with open(yf) as f:
             metadata = yaml.load(f, Loader=yaml.FullLoader)
@@ -20,14 +20,21 @@ def test_all_yaml_files():
 def test_dataset():
     "Check basic information in dataset files."
     for yf in yaml_files:
-        folder_name = yf.split("/")[1]
-        file_name = "datasets/{0}/{0}.tsv.gz".format(folder_name)
+        folder_name = yf.split("/")[-2]
+        file_name = yf.split('metadata.yaml')[0]+folder_name+'.tsv.gz'
         with open(yf) as f:
             metadata = yaml.load(f, Loader=yaml.FullLoader)
         dataset = pd.read_csv(file_name, sep='\t', compression='gzip')
-        cols = sorted(list(dataset.columns))
+        cols = set(list(dataset.columns))
 
-        features = [s['name'] for s in metadata['features']]
-        exported_cols = sorted(features + ['target'])
+        exported_cols = set([str(s['name']) for s in metadata['features']]
+                +['target'])
+
+        if exported_cols != cols:
+            print('filename:',file_name)
+            print('metadata:',metadata)
+            print('exported_cols:',exported_cols)
+            print('cols:',cols)
+            print('set diff:',cols.difference(exported_cols))
 
         assert exported_cols == cols
