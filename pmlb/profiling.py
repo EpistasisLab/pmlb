a6b6ef4e7224edbe4a56775aea3abb0217875b60
Trang Le <grixor@gmail.com>
2020-07-23 20:50:47 -0400

    install pandas-profiling in CI update profiling.py

--- pmlb/profiling.py
+++ pmlb/profiling.py
@@ -5,27 +5,25 @@ import pathlib
 
 def make_profiling(dataset, write_dir, dat_dir='datasets/'):
     df = fetch_data(dataset, local_cache_dir=dat_dir)
-    write_path = pathlib.Path(write_dir, dataset + '.html')
+    write_path = write_dir.joinpath(dataset + '.html')
     
-    profile = ProfileReport(
-        df, title=dataset, minimal=True, explorative=True)
+    profile = ProfileReport(df, title=dataset, explorative=True)
     
     profile.to_file(write_path)
 
 
 if __name__ =='__main__':
 
-    write_dir = 'docs_sources/profilings/'
-    p = pathlib.Path(write_dir)
-    if not p.exists():
-        p.mkdir(parents=True)
+    # write_dir = 'docs_sources/profile/'
+    write_dir = pathlib.Path('docs_sources/profile/')
+    write_dir.mkdir(exist_ok=True)
 
-    updated_datasets = get_updated_datasets()
+    # updated_datasets = get_updated_datasets()
 
-    for dataset in dataset_names[11:13]:
-        write_path = pathlib.Path(write_dir, dataset + '.html')
+    for dataset in dataset_names[:30]:
+        write_path = write_dir.joinpath(dataset + '.html')
 
-        if (dataset not in updated_datasets and write_path.exists()):
-            # don't update if the dataset has not changed
-            continue
+        # if (dataset not in updated_datasets and write_path.exists()):
+        #     # don't update if the dataset has not changed
+        #     continue
         make_profiling(dataset, write_dir)
